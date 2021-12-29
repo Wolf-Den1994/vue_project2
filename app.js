2e52f3035e08e48bd863c799c0a55840ae22618e
Denis <wolf-den@tut.by>
2021-12-29 18:52:27 +0300

    computed

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ Vue.createApp({
       lastName: 'Dev',
       age: 27
     },
-    items: [1,2,3,4,5,6],
-  })
+    items: [1, 2, 3, 4, 5, 6],
+  }),
+  computed: {
+    evenItems() {
+      return this.items.filter((item) => item % 2 === 0)
+    }
+  }
 }).mount('#app')
